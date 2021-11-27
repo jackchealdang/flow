f98b73d5cd7c02f0ca2476e46ea6da794d799e41
Jackcheal Dang <jackchealdang@gmail.com>
2021-11-27 14:34:13 -0600

    removed JS files

--- packages/renderer/src/app.tsx
+++ packages/renderer/src/app.tsx
@@ -1,20 +1,16 @@
-import React, { useState, useCallback } from 'react'
-import Editor from './editor'
-import Preview from './preview'
-import './app.css'
-
-const App: React.FC = () => {
-  const [doc, setDoc] = useState<string>('# Hello, World!\n')
-
-  const handleDocChange = useCallback(newDoc => {
-    setDoc(newDoc)
-  }, [])
-  return (
-    < div className='app' >
-      <Editor onChange={handleDocChange} initialDoc={doc} />
-      <Preview doc={doc} />
-    </div >
-  )
-}
-
-export default App
+import React, { useState, useCallback } from 'react';
+import Editor from './editor';
+import Preview from './preview';
+import './app.css';
+const App = () => {
+    const [doc, setDoc] = useState('# Hello, World!\n');
+    const handleDocChange = useCallback(newDoc => {
+        setDoc(newDoc);
+    }, []);
+    return (<div className='app'>
+      <Editor onChange={handleDocChange} initialDoc={doc}/>
+      <Preview doc={doc}/>
+    </div>);
+};
+export default App;
+//# sourceMappingURL=app.js.map
